5a10e1915a858dc26c2d80af596bdc8a3336a56a
hxcyon <halxcyon@gmail.com>
2021-05-02 13:59:13 +0900

    Initial Commit

---
+++ XcodeForkServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XcodeForkServer
+//
+//  Created by hxcyon on 2/5/21.
+//  Copyright © 2021 Hye In Kim. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
